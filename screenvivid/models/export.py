9ea6a9b6d79e2af2aaa30a8f4f9a4d7b89365c81
Beckett Whritenour <39536218+whrit@users.noreply.github.com>
2025-09-09 18:40:36 -0400

    Add integration test for export click highlights

--- screenvivid/models/export.py
+++ screenvivid/models/export.py
@@ -7,6 +7,8 @@ import threading
 import numpy as np
 from PIL import Image
 from PySide6.QtCore import Signal, QThread
+
+from screenvivid.models.utils import transforms
 from screenvivid.utils.general import get_os_name, get_ffmpeg_path
 from screenvivid.utils.logging import logger
 
@@ -20,6 +22,33 @@ class VideoReaderThread(QThread):
         self.stop_flag = stop_flag
         self.export_params = export_params
 
+        # Build transform pipeline similar to VideoControllerModel
+        self.transforms = transforms.Compose({
+            "aspect_ratio": transforms.AspectRatio(
+                export_params.get("aspect_ratio", "Auto"),
+                export_params.get("screen_size", (video_processor.frame_width, video_processor.frame_height)),
+            ),
+            "auto_zoom": transforms.AutoZoom(
+                move_data=export_params.get("move_data", {}),
+                zoom_factor=export_params.get("zoom_factor", 2.0),
+                keyframes=export_params.get("zoom_keyframes", {}),
+            ) if export_params.get("auto_zoom_enabled", False) else transforms.Identity(),
+            "click_highlight": transforms.ClickHighlight(
+                click_data=export_params.get("click_data", []),
+                color=export_params.get("highlight_color"),
+                radius=export_params.get("highlight_radius", 20),
+            ) if export_params.get("highlight_enabled", False) else transforms.Identity(),
+            "cursor": transforms.Cursor(
+                move_data=export_params.get("move_data", {}),
+                cursors_map=export_params.get("cursors_map", {}),
+                offsets=export_params.get("offsets", (None, None)),
+                scale=export_params.get("cursor_scale", 1.0),
+            ),
+            "padding": transforms.Padding(export_params.get("padding", 0)),
+            "border_shadow": transforms.BorderShadow(border_radius=export_params.get("border_radius", 0)),
+            "background": transforms.Background(export_params.get("background", {"type": "color", "value": "#000000"})),
+        })
+
     def run(self):
         output_size = tuple(self.export_params.get("output_size"))
 
@@ -28,21 +57,22 @@ class VideoReaderThread(QThread):
         self.video_processor.current_frame = self.video_processor.start_frame
 
         total_frames = self.video_processor.end_frame - self.video_processor.start_frame
-        for i in range(total_frames):
+        for _ in range(total_frames):
             if self.stop_flag.is_set():
                 break
 
             ret, frame = self.video_processor.video.read()
             if ret:
-                # Get processed frame (in RGB format)
-                processed_frame = self.video_processor.process_frame(frame)
+                processed_frame = self.transforms(
+                    input=frame,
+                    start_frame=self.video_processor.start_frame + self.video_processor.current_frame,
+                )
+                processed_frame = cv2.cvtColor(processed_frame, cv2.COLOR_BGR2RGB)
                 self.video_processor.current_frame += 1
 
-                # Resize frame if necessary
                 if processed_frame.shape[:2] != output_size:
                     processed_frame = cv2.resize(processed_frame, output_size)
 
-                # Push the processed frame to the queue
                 self.frame_queue.put(processed_frame)
             else:
                 break
@@ -50,7 +80,6 @@ class VideoReaderThread(QThread):
         self.video_processor.video.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
         self.video_processor.current_frame = current_frame
 
-        # Signal that the reading is done
         self.frame_queue.put(None)
 
 # Codec-specific configurations
@@ -267,6 +296,7 @@ class FFmpegWriterThread(QThread):
             finally:
                 self.finished.emit()
 
+
 class ExportThread(QThread):
     progress = Signal(float)
     finished = Signal()
@@ -274,15 +304,45 @@ class ExportThread(QThread):
     def __init__(self, video_processor, export_params):
         super().__init__()
         self.video_processor = video_processor
-        self.export_params = export_params
-        # Increased queue size for better buffering
-        self.frame_queue = queue.Queue(maxsize=90)  # Buffer 3 second at 30fps
+
+        # Make a copy of export parameters and enrich with runtime configuration
+        self.export_params = dict(export_params)
+        vp = self.video_processor
+        self.export_params.setdefault("fps", vp.fps)
+        self.export_params.setdefault("output_size", (vp.frame_width, vp.frame_height))
+        self.export_params.setdefault("aspect_ratio", vp.aspect_ratio)
+        self.export_params.setdefault("padding", vp.padding)
+        self.export_params.setdefault("border_radius", vp.border_radius)
+        self.export_params.setdefault("background", vp.background)
+        self.export_params.setdefault("cursor_scale", vp.cursor_scale)
+        self.export_params.setdefault("highlight_enabled", vp.highlight_enabled)
+        self.export_params.setdefault("highlight_color", vp.highlight_color)
+        self.export_params.setdefault("highlight_radius", vp.highlight_radius)
+        self.export_params.setdefault("auto_zoom_enabled", vp.auto_zoom_enabled)
+        self.export_params.setdefault("zoom_factor", vp._zoom_factor)
+        self.export_params.setdefault("move_data", vp._mouse_events)
+        self.export_params.setdefault("click_data", vp._click_events)
+        self.export_params.setdefault("zoom_keyframes", vp._zoom_keyframes)
+        self.export_params.setdefault("cursors_map", vp._cursors_map)
+        self.export_params.setdefault("offsets", (vp._x_offset, vp._y_offset))
+        if vp._transforms is not None and vp._transforms.get("aspect_ratio"):
+            self.export_params.setdefault("screen_size", vp._transforms["aspect_ratio"].screen_size)
+        else:
+            self.export_params.setdefault("screen_size", (vp.frame_width, vp.frame_height))
+
+        # Cache cursor positions for quick lookup
+        move_data = self.export_params.get("move_data", {})
+        if isinstance(move_data, list):
+            self.export_params["move_data"] = {i: v for i, v in enumerate(move_data)}
+
+        # Queue used between reader and writer threads
+        self.frame_queue = queue.Queue(maxsize=90)
         self._stop_flag = threading.Event()
 
-        self.export_params["total_frames"] = self.video_processor.end_frame - self.video_processor.start_frame
+        self.export_params["total_frames"] = vp.end_frame - vp.start_frame
 
-        self.reader_thread = VideoReaderThread(video_processor, self.frame_queue, self._stop_flag, export_params)
-        self.writer_thread = FFmpegWriterThread(self.frame_queue, self._stop_flag, export_params)
+        self.reader_thread = VideoReaderThread(vp, self.frame_queue, self._stop_flag, self.export_params)
+        self.writer_thread = FFmpegWriterThread(self.frame_queue, self._stop_flag, self.export_params)
 
         self.writer_thread.progress.connect(self.progress.emit)
         self.writer_thread.finished.connect(self.finished.emit)
